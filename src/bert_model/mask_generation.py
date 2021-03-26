9ff10ddd36ae5000301d0f741826a40c820d062f
Colby Ham <colby.ham@pnnl.gov>
2021-03-26 11:15:34 -0700

    Cleanup bert_model

--- src/bert_model/mask_generation.py
+++ src/bert_model/mask_generation.py
@@ -1,7 +1,5 @@
 import random
 
-import torch
-
 
 class GenerateGraphMask:
     """
@@ -9,12 +7,13 @@ class GenerateGraphMask:
     each
     edge.tensor = cat(src_node.tensor, dest_node.tensor, relation.tensor)
     Provides fuctions to generate:
-        link preiction task for 
+        link preiction task for
         1) subgraph edge prediction
         2) edge.source prediction
         3) edge.relation prediction
         4) edge.target prediction
     """
+
     def __init__(self, subgraph_sequences, n_pred):
         self.subgraph_sequences = subgraph_sequences
         self.n_pred = n_pred
@@ -26,14 +25,14 @@ class GenerateGraphMask:
         mask = []
         for subgraph in self.subgraph_sequences:
             num_edges = len(subgraph)
-            #print(num_edges, self.n_pred)
+            # print(num_edges, self.n_pred)
             edge_mask_index = random.sample(range(num_edges), self.n_pred)
             subgraph_mask = []
             for i in range(len(subgraph)):
                 if i in edge_mask_index:
-                    subgraph_mask.append((1,1,0))
+                    subgraph_mask.append((1, 1, 0))
                 else:
-                    subgraph_mask.append((1,1,1))
+                    subgraph_mask.append((1, 1, 1))
             mask.append(subgraph_mask)
         return mask
 
@@ -41,16 +40,16 @@ class GenerateGraphMask:
         """
         generate source mask
         """
-        mask = []        
+        mask = []
         for subgraph in self.subgraph_sequences:
             num_edges = len(subgraph)
             edge_mask_index = random.sample(range(num_edges), self.n_pred)
             subgraph_mask = []
             for i in range(len(subgraph)):
                 if i in edge_mask_index:
-                    subgraph_mask.append((0,1,1))
+                    subgraph_mask.append((0, 1, 1))
                 else:
-                    subgraph_mask.append((1,1,1))
+                    subgraph_mask.append((1, 1, 1))
             mask.append(subgraph_mask)
         return mask
 
@@ -65,26 +64,26 @@ class GenerateGraphMask:
             subgraph_mask = []
             for i in range(len(subgraph)):
                 if i in edge_mask_index:
-                    subgraph_mask.append((1,0,1))
+                    subgraph_mask.append((1, 0, 1))
                 else:
-                    subgraph_mask.append((1,1,1))
+                    subgraph_mask.append((1, 1, 1))
             mask.append(subgraph_mask)
         return mask
-    
+
     def MaskGeneration(self, mask_type):
-        if mask_type == 'relation':
+        if mask_type == "relation":
             input_mask = self.relation_prediction()
-        elif mask_type == 'source':
+        elif mask_type == "source":
             input_mask = self.source_prediction()
-        elif mask_type == 'target':
+        elif mask_type == "target":
             input_mask = self.target_prediction()
         else:
-            print('\n Wrong mask type. No mask is generated!')
+            print("\n Wrong mask type. No mask is generated!")
             input_mask = []
             for subgraph in self.subgraph_sequences:
-                subgraph_mask = [(1,1,1) for i in range(len(subgraph))]
+                subgraph_mask = [(1, 1, 1) for i in range(len(subgraph))]
                 input_mask.append(subgraph_mask)
-        
+
         return input_mask
 
     def GCN_MaskGeneration(self):
@@ -97,7 +96,7 @@ class GenerateGraphMask:
             subgraph_mask = []
             subgraph_masked_nodes = []
             subgraph_masked_position = []
-            for i in range(len(subgraph)):
+            for i, _ in enumerate(subgraph):
                 if i in mask_index:
                     subgraph_mask.append(0)
                     subgraph_masked_nodes.append(subgraph[i])
@@ -109,4 +108,3 @@ class GenerateGraphMask:
             masked_position.append(subgraph_masked_position)
 
         return mask, masked_nodes, masked_position
-
--- src/bert_model/process_walks_gcn.py
+++ src/bert_model/process_walks_gcn.py
@@ -1,16 +1,10 @@
-import argparse
 import json
 import os
-import re
 
-import networkx as nx
-import pandas as pd
 import torch
 from torch.autograd import Variable
 
 from src.bert_model.mask_generation import GenerateGraphMask
-from src.processing.attributed_graph import AttributedGraph
-from src.processing.context_generator import ContextGenerator
 
 
 def get_normalized_masked_ids(masked_node_ids, ent2id):
@@ -19,18 +13,19 @@ def get_normalized_masked_ids(masked_node_ids, ent2id):
     returns : [batch * list(normalized_node_id) ] #len(attr-graph.get_number_of_nodes)]
     """
 
-    normalized_node_ids= list()
+    normalized_node_ids = list()
     for batch_node_ids in masked_node_ids:
-        normalized_node_ids_batch = [ent2id[str(x.cpu().numpy().tolist())] \
-            for x in batch_node_ids]
+        normalized_node_ids_batch = [
+            ent2id[str(x.cpu().numpy().tolist())] for x in batch_node_ids
+        ]
         normalized_node_ids.append(normalized_node_ids_batch)
-        
+
     return torch.LongTensor(normalized_node_ids)
 
+
 class Processing_GCN_Walks:
-    def __init__(self, nodeid2rowid, relations, 
-        n_pred, max_length, max_pred):
-        
+    def __init__(self, nodeid2rowid, relations, n_pred, max_length, max_pred):
+
         self.nodeid2rowid = nodeid2rowid
         self.relations = relations
         self.n_pred = n_pred
@@ -42,40 +37,40 @@ class Processing_GCN_Walks:
         no_nodes = 0
         for node_type in self.nodeid2rowid:
             no_nodes += len(self.nodeid2rowid[node_type])
-        special_tokens_set = ['[PAD]', '[MASK]']
-        for ii in range(len(special_tokens_set)):
+        special_tokens_set = ["[PAD]", "[MASK]"]
+        for ii, _ in enumerate(special_tokens_set):
             self.special_tokens[special_tokens_set[ii]] = no_nodes + ii
-        
+
         return self.special_tokens
-    
+
     def masking_and_padding(self, subgraphs_list):
-        for ii in range(len(subgraphs_list)):
-            #each subgraph has max_length edges
-            subgraphs_list[ii] = subgraphs_list[ii][:self.max_length+1]
-            
-        #generate mask
+        for ii, _ in enumerate(subgraphs_list):
+            # each subgraph has max_length edges
+            subgraphs_list[ii] = subgraphs_list[ii][: self.max_length + 1]
+
+        # generate mask
         graph_mask = GenerateGraphMask(subgraphs_list, self.n_pred)
         input_mask, masked_nodes, masked_position = graph_mask.GCN_MaskGeneration()
         # print('\n input mask:')
         # for ii in range(len(input_mask)):
         #     print(input_mask[ii])
 
-        for ii in range(len(subgraphs_list)):
+        for ii, _ in enumerate(subgraphs_list):
             for jj in range(len(subgraphs_list[ii])):
                 if input_mask[ii][jj] == 0:
-                    subgraphs_list[ii][jj] = self.special_tokens['[MASK]']
-            #padding subgraph to max_length
-            if len(subgraphs_list[ii]) < self.max_length+1:
+                    subgraphs_list[ii][jj] = self.special_tokens["[MASK]"]
+            # padding subgraph to max_length
+            if len(subgraphs_list[ii]) < self.max_length + 1:
                 n_pad = self.max_length + 1 - len(subgraphs_list[ii])
-                for nn in range(n_pad):
-                    subgraphs_list[ii].append(self.special_tokens['[PAD]'])
-        
+                for _ in range(n_pad):
+                    subgraphs_list[ii].append(self.special_tokens["[PAD]"])
+
         return subgraphs_list, masked_nodes, masked_position
-    
+
     def process_minibatch(self, data_path, data_name, task, batch_id):
-        folder = os.path.join(data_path, task) 
-        file_name = data_name+'_'+task+'_batch_'+str(batch_id)+'.txt'
-        fp = open(os.path.join(folder, file_name), 'r')
+        folder = os.path.join(data_path, task)
+        file_name = data_name + "_" + task + "_batch_" + str(batch_id) + ".txt"
+        fp = open(os.path.join(folder, file_name), "r")
         subgraphs = []
         cnt = 0
         for line in fp:
@@ -98,7 +93,7 @@ class Processing_GCN_Walks:
                     nodes.append(int(target_node))
             all_nodes.append(nodes)
 
-        special_tokens = self.getSpecialTokens()
+        # special_tokens = self.getSpecialTokens()
         all_nodes, masked_nodes, masked_postion = self.masking_and_padding(all_nodes)
         # print('\n subgraphs \n', subgraphs)
         # print('\n all_nodes \n', all_nodes)
@@ -107,16 +102,15 @@ class Processing_GCN_Walks:
 
         masked_nodes = Variable(torch.LongTensor(masked_nodes))
         masked_postion = Variable(torch.LongTensor(masked_postion))
-        
+
         return subgraphs, all_nodes, masked_nodes, masked_postion
 
-    
     def pad_max_seq_len(self, subgraphs_list, max_seq_len):
         padded_subgraphs = []
         for subgraph in subgraphs_list:
             subgraph_actual_len = len(subgraph)
-            for nn in range(max_seq_len-subgraph_actual_len):
-                subgraph.append(self.special_tokens['[PAD]'])
+            for _ in range(max_seq_len - subgraph_actual_len):
+                subgraph.append(self.special_tokens["[PAD]"])
             padded_subgraphs.append(subgraph)
 
         return padded_subgraphs
@@ -129,9 +123,9 @@ class Processing_GCN_Walks:
         [[source, target, relation, 0/1]]
         The last value indicate true or false edge
         """
-        folder = os.path.join(finetune_path, task) 
-        file_name = data_name+'_'+task+'_batch_'+str(batch_id)+'.txt'
-        fp = open(os.path.join(folder, file_name), 'r')
+        folder = os.path.join(finetune_path, task)
+        file_name = data_name + "_" + task + "_batch_" + str(batch_id) + ".txt"
+        fp = open(os.path.join(folder, file_name), "r")
         subgraphs = []
         labels = []
         cnt = 0
@@ -145,7 +139,7 @@ class Processing_GCN_Walks:
             #     break
         fp.close()
 
-        #print("In fine tuning process batch, loaded walks", all_walks)
+        # print("In fine tuning process batch, loaded walks", all_walks)
         all_nodes = []
         for subgraph in subgraphs:
             nodes = []
@@ -158,13 +152,11 @@ class Processing_GCN_Walks:
                     nodes.append(target_node)
             all_nodes.append(nodes)
 
-        special_tokens = self.getSpecialTokens()
-        all_nodes = self.pad_max_seq_len(all_nodes, self.max_length+1)
-        #print("created rowid subgraphs for batch", subgraphs_list, subgraph_len)
+        # special_tokens = self.getSpecialTokens()
+        all_nodes = self.pad_max_seq_len(all_nodes, self.max_length + 1)
+        # print("created rowid subgraphs for batch", subgraphs_list, subgraph_len)
         labels = Variable(torch.FloatTensor(labels)).unsqueeze(1)
-        #print(subgraphs, labels)
+        # print(subgraphs, labels)
         # print(labels)
-        
+
         return subgraphs, all_nodes, labels
-    
-    
